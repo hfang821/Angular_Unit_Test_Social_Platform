1835f75eeedfe1f9d81f8dc919d7618013edeac5
Kevin Fang <kevin-fang@hotmail.ca>
2024-08-25 13:26:39 -0400

    create a user service test to test the getUsers service

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -3,14 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
+
   let service: UserService;
 
+  // before each test, create a new instance of UserService
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
+  it('should get created', () => {
+    // check if the service is not a faulty value 
     expect(service).toBeTruthy();
   });
+
+  it('should get users', () =>{
+    service.getUsers().subscribe(users => {
+      expect(users.length).toBeGreaterThan(0);
+    })
+  })
 });
