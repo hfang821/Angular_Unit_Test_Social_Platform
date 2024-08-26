a560d579f48d791ddfe49eb5781be0fc3a81af01
Kevin Fang <kevin-fang@hotmail.ca>
2024-08-25 23:31:13 -0400

    prepare the dependencies and create the spyon to return the mock data

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,18 +1,35 @@
+// TestBed: A utility for testing Angular applications. It is used to create a module for testing and create services, components, and pipes for testing.
+// ComponentFixure: A class that is used to create a fixture for a component. It is used to create a fixture for a component and provides access to the component instance and the component's DOM.
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserService } from '../user.service';
+import { of } from 'rxjs';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
+  // fixure is like a wrapper for component, which has additional functionalities which we can call.
   let fixture: ComponentFixture<UserListComponent>;
+  let userService: UserService;
+  let userServiceSpy: jasmine.Spy;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserListComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      // declarations: [UserListComponent] is used to declare the component which we want to test. (specifically to angular components)
+      declarations: [UserListComponent],
+      providers: [UserService]
+    }).compileComponents();
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // mannually trigger change detection to update the data binding
+    // fixture.detectChanges();
+    userService = TestBed.inject(UserService);
+    // spyOn is used to mock the service method and return a value
+    userServiceSpy = spyOn(userService, 'getUsers').and.returnValue(of([
+      {id: 1, name: 'john'},
+      {id: 2, name: 'Maria'},
+      {id: 3, name: 'Kevin'}
+    ]))
   });
 
   it('should create', () => {
